6d2eac107e68934ed0a137ffa068ded0bde5003a
LHW <1055807337@qq.com>
2022-05-08 16:46:11 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view />
+    <FooterGuide v-show="$route.meta.showFooter" />
+  </div>
+</template>
+
+<script>
+// 引入组件
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+export default {
+  name: 'App',
+  // 注册组件
+  components: {
+    FooterGuide
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+.app
+  width 100%
+  height 100%
+  background #f5f5f5
+</style>
---
+++ src/new.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'new'
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped></style>
